a90f5642dbdb58ef578f14218565db88786e8372
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-02 19:46:13 +0000

    get software engineers

--- src/main/java/com/firstapp1/SoftwareEngineerController.java
+++ src/main/java/com/firstapp1/SoftwareEngineerController.java
@@ -1,4 +1,24 @@
 package com.firstapp1;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers(){
+        return List.of(
+                new SoftwareEngineer(
+                        1,"James","js, node, react, tailwind-css"
+                ),
+                new SoftwareEngineer(
+                        2,"Jaimie","Java, spring, spring boot"
+                )
+        );
+    }
 }
